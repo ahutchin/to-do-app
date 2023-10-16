3e29bf6b8713ed0b79790c881a993ed8e9065851
Alexander Hutchinson <ahutchin2003@gmail.com>
2023-10-15 21:55:17 -0400

    first commit

---
+++ client/src/components/ListHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ListHeader = ({ listName }) => {
+
+  const signOut = () => {
+    console.log("signout");
+  };
+
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create">ADD NEW</button>
+        <button className="signout" onCLick={signOut}>
+          SIGN OUT
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ListHeader;
